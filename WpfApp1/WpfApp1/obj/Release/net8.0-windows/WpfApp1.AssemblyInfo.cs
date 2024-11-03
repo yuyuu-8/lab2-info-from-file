f552ba335c36f3a923e271fecbab72343d5dc775
yuyu <sishichulu@gmail.com>
2024-11-03 23:01:18 +0300

    feat: add ".tiff"

--- WpfApp1/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f02ac5e233aa77c28a022f7896f02da1e5cd7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
